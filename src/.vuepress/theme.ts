ad098b6d6bec7589fac461f451d0799c8173ae4b
galifans <55650639+galifans@users.noreply.github.com>
2026-08-23 22:29:10 +0800

    chore: 版权署名更新为 WikiAndroid-TC

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -8,7 +8,7 @@ export default hopeTheme({
 
   // 作者信息
   author: {
-    name: "WikiAndroid",
+    name: "WikiAndroid-TC",
   },
 
   // 图标库（不依赖网络字体，使用内置样式）
@@ -55,7 +55,7 @@ export default hopeTheme({
     readingTime: true,
     // 版权信息
     copyright: {
-      author: "WikiAndroid",
+      author: "WikiAndroid-TC",
       license: "MIT",
       global: false,
     },
